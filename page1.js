1863f21793b02456c6ad9349fd5790448460357d
Botyyyy <107973574+DeveloperPrabit@users.noreply.github.com>
2024-10-19 07:42:52 +0545

    Update page1.js

--- page1.js
+++ page1.js
@@ -1,27 +1,16 @@
-import firebase from 'firebase/app';
-import 'firebase/auth';
 import { firebaseConfig } from "./config.js";
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
-
 const signoutBtn = document.querySelector('#signoutbtn');
 
 signoutBtn.addEventListener('click', () => {
-  auth.signOut()
-    .then(() => {
-      console.log('User signed out successfully');
-      location.href = "index.html";
-    })
-    .catch((error) => {
-      alert('Error signing out: ' + error.message);
-    });
-});
-
-// Check if user is signed in
-auth.onAuthStateChanged(user => {
-  if (!user) {
-    // If no user is signed in, redirect to the login page
-    location.href = "index.html";
-  }
+    auth.signOut()
+        .then(() => {
+            console.log('User signed out successfully');
+            location.href = "index.html"; // Redirect to login page
+        })
+        .catch((error) => {
+            alert('Error signing out: ', error);
+        });
 });
